c21b25dcbeca76713ecf8b445a507b20cfd9efcf
Krishna Panicker <krishna.panicker2006@gmail.com>
2025-11-06 00:01:23 -0500

    Testing if Pick'em page displays after signing in

--- src/pages/PickEm.jsx
+++ src/pages/PickEm.jsx
@@ -2,6 +2,11 @@
 
 
 function PickEm() {
+    return (
+        <div>
+            <h1 className="text-3xl text-white">If you can see this then it works. Yes it works.</h1>
+        </div>
+    )
 }
 
 export default PickEm
